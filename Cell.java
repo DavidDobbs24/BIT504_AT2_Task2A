df8813542d7b5d6645d12cd6c7282066494a097c
DavidDobbs24 <168958310+DavidDobbs24@users.noreply.github.com>
2024-06-01 21:39:57 +1200

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -11,13 +11,9 @@ public class Cell {
 	
 	/** Constructor to initialise this cell with the specified row and col */
 	public Cell(int row, int col) {
-		
-		// TODO: Initialise the variables row, col 
-
-		
-		
-		//TODO: call the method that sets the cell content to EMPTY
-		 
+		this.row = row;
+		this.col = col;
+		clear(); // Intialize the cell content to empty	 
 	}
 	
 
@@ -44,10 +40,6 @@ public class Cell {
 	
 	/** Set this cell's content to EMPTY */
 	public void clear() {
-		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
-		
-	}
-		
+		content = Player.Empty;
+	}		
 }
