c4e29a6c21e74c243f35db458a2a9bfa8991b393
DavidDobbs24 <168958310+DavidDobbs24@users.noreply.github.com>
2024-06-01 22:10:53 +1200

    QC Player.java

--- Player.java
+++ Player.java
@@ -1,9 +1,6 @@
-/**
- * Enumeration for the players move
- */
-
+/*** Enumeration for the players' moves. This enum defines the possible states of a cell in the Tic-Tac-Toe game.***/
 public enum Player {
-	Empty, //Represents an empty cell
-	Cross, // Represents a cell occupied by a cross (X)
-	Nought, // Represents a cell occupied by a nought (O)
+    Empty,  // Represents an empty cell
+    Cross,  // Represents a cell occupied by a cross (X)
+    Nought  // Represents a cell occupied by a nought (O)
 }
